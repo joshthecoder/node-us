630775d0439a2d3ef1c567b5a41d7d799af4aa0d
Josh Roesslein <jroesslein@gmail.com>
2010-08-28 14:12:08 -0500

    Implement code to pull tweets from Twitter via the streaming API.

--- server.js
+++ server.js
@@ -9,14 +9,36 @@
 var connect = require('connect');
 var socketio = require('socket.io.js');
 var twitter = require('twitter-node');
+var log = require('sys').log;
 
 // Server options...
 var PUBLIC_PATH = __dirname + '/public';
 var HTTP_PORT = 8080;
+var TWITTER_CREDENTIALS = {
+    user: 'tweepytest',
+    password: 'josh123'
+}
 
 // Setup a HTTP server to serve both our static and streaming content.
 var httpserver = connect.createServer(
     connect.staticProvider(PUBLIC_PATH)
 );
-
 httpserver.listen(HTTP_PORT);
+
+// Setup up our listening socket that will deliver the tweet stream.
+var tweetstream = io.listen(httpserver, {resource: 'tweetstream'});
+
+// Establish a sample stream connection with Twitter.
+var tweetsampler = new twitter.TwitterNode(TWITTER_CREDENTIALS);
+tweetsampler.action = 'sample';
+tweetsampler.on('tweet', function(tweet) {
+    if (tweet.geo) {
+        log("got geo! " + tweet.geo);
+    } else {
+        // TODO: try looking up user's location
+    }
+});
+tweetsampler.on('error', function(error) {
+    log('Tweet sampler error: ' + error);
+});
+tweetsampler.stream();
